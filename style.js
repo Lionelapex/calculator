5c6bf1a4866848631947de0b20276848879ec375
Lionelapex <126233514+Lionelapex@users.noreply.github.com>
2024-02-14 01:01:28 +0200

    Add files via upload

--- style.js
+++ style.js
@@ -1,10 +1,10 @@
 const buttons=document.querySelectorAll("button.themebtn")
 
-buttons.forEach(button=>{
-    const[btn1,btn2,btn3]=buttons
+
+ const[btn1,btn2,btn3]=buttons
 
    btn1.addEventListener("click",()=>{
-    btn1.style="background-color: rgb(196, 71, 13);"
+    btn1.style="background-color: rgb(9, 82, 82);"
     btn2.style="background-color: black;"
     btn3.style="background-color: black;"
    
@@ -12,13 +12,12 @@ buttons.forEach(button=>{
 
     btn2.addEventListener("click",()=>{
         btn1.style="background-color: black;"
-        btn2.style="background-color: rgb(196, 71, 13);"
+        btn2.style="background-color: rgb(9, 82, 82);"
         btn3.style="background-color: black;"
     })
 
     btn3.addEventListener("click",()=>{
         btn1.style="background-color: black;"
         btn2.style="background-color: black;"
-        btn3.style="background-color: rgb(196, 71, 13);"
+        btn3.style="background-color: rgb(9, 82, 82);"
     })
-})
